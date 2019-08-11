b4b7ddaf002f4f43f974ed8f17c495abe3201431
Omar Sherif <omar.sherifs@live.com>
2019-08-11 19:33:53 +0200

    Basic Structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import Todos from './componants/Todos'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+
+  state = {
+    Todos: [
+      {
+        id:1,
+        title:"Finish Home Screen",
+        completed: false,
+      },
+      {
+        id:2,
+        title:"Upload you new vlog",
+        completed: false,
+      },
+    ]
+  }
+
+  render(){
+      return(
+        <div className="App">
+        <Todos Todos={this.state.Todos} />
+      </div>
+      );
+  }
 }
 
 export default App;
